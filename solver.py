ff39322cdd3695e53e8160766feb34cbcbd9bb3e
Yichi Zhang <z5589923@ad.unsw.edu.au>
2025-10-27 11:27:02 +1100

    finished more

--- solver.py
+++ solver.py
@@ -105,6 +105,46 @@ def hidden_singles(possibles):
                                 rm_from_rows(possibles, r, c, x)
                                 rm_from_cols(possibles, r, c, x)
                                 rm_from_subgrid(possibles, r, c, x)
+
+    return possibles
+
+
+def col_hidden_singles(possibles):
+    # hidden singles for columns
+    # check every column
+    for col in range(0, 9):
+        H = {n: 0 for n in range(1, 10)}
+        for row in range(0, 9):
+            S = possibles[row][col]
+            for num in S:
+                H[num] += 1
+        for row in range(0, 9):
+            S = possibles[row][col]
+            for num in S:
+                if H[num] == 1 and len(S) != 1:
+                    possibles[row][col] = {num}
+                    rm_from_rows(possibles, row, col, num)
+                    rm_from_cols(possibles, row, col, num)
+                    rm_from_subgrid(possibles, row, col, num)
+    return possibles
+
+def row_hidden_singles(possibles):
+    # hidden singles for columns
+    # check every column
+    for row in range(0, 9):
+        H = {n: 0 for n in range(1, 10)}
+        for col in range(0, 9):
+            S = possibles[row][col]
+            for num in S:
+                H[num] += 1
+        for col in range(0, 9):
+            S = possibles[row][col]
+            for num in S:
+                if H[num] == 1 and len(S) != 1:
+                    possibles[row][col] = {num}
+                    rm_from_rows(possibles, row, col, num)
+                    rm_from_cols(possibles, row, col, num)
+                    rm_from_subgrid(possibles, row, col, num)
     return possibles
 
 def y_wing_search(pr, pc):
@@ -130,63 +170,6 @@ def y_wing_search(pr, pc):
     # print(subgrid_viewers)
     return row_viewers | col_viewers | subgrid_viewers
 
-def y_wing_rm(possibles, sees_both, to_rm, exclusions):
-    for b_row, b_col in sees_both:
-        if (b_row, b_col) in exclusions:
-            continue
-
-        if sees_both == {(8, 4), (7, 7), (8, 3), (7, 6), (8, 5), (7, 8)}:
-            print(possibles[b_row][b_col], end = ",")
-            # print(to_rm)
-        if to_rm & possibles[b_row][b_col] and len(possibles[b_row][b_col]) != 1:
-
-            # print(possibles[b_row][b_col])
-            print(to_rm)
-            possibles[b_row][b_col] = possibles[b_row][b_col] - to_rm
-            # print(possibles[b_row][b_col])
-            if len(possibles[b_row][b_col]) == 1:
-                next_rm = next(iter(possibles[b_row][b_col]))
-                rm_from_subgrid(possibles, b_row, b_col, next_rm)
-                rm_from_rows(possibles, b_row, b_col, next_rm)
-                rm_from_cols(possibles, b_row, b_col, next_rm)
-    # return possibles
-# def y_wing(possibles):
-#     # Iterate over every cell as the pivot (2 candidates)
-#     for pr in range(9):
-#         for pc in range(9):
-#             pivot = possibles[pr][pc]
-#             if len(pivot) != 2:
-#                 continue
-#             # print(pivot)
-
-#                 # print(j_shared)
-#             wings = list(y_wing_search(pr, pc))
-#             # if (pr, pc) == (7, 8):
-#             #     print([possibles[r][c] for r, c in wings])
-#             for i in range(0, len(wings)):
-#                 for j in range(i + 1, len(wings)):
-#                     i_row, i_col = wings[i]
-#                     i_wing = possibles[i_row][i_col]
-#                     j_row, j_col = wings[j]
-#                     j_wing = possibles[j_row][j_col]
-#                     if len(i_wing) != 2 or len(j_wing) != 2:
-#                         # print(f"{str(i_wing)}, +  {str(j_wing)}")
-#                         continue
-#                     i_shared = i_wing & pivot
-#                     j_shared = j_wing & (pivot - i_shared)
-#                     # if i_wing == {5, 8} and j_wing == {8, 9}:
-#                     #     print(i_shared)
-#                     #     print(j_shared)
-#                     if len(i_shared) == 1 and len(j_shared) == 1:
-#                         sees_both = y_wing_search(i_row, i_col) & y_wing_search(j_row, j_col)
-
-#                         to_rm = i_wing & j_wing
-#                         # if i_wing == {5, 8} and j_wing == {8, 9}:
-#                         #     print(sees_both)
-#                         #     print(to_rm)
-#                         print(f"{str(i_wing)}, +  {str(j_wing)}")
-#                         y_wing_rm(possibles, sees_both, to_rm, {(pr, pc)})
-#     return possibles
 
 def y_wing(possibles):
     for pr in range(9):
@@ -231,6 +214,11 @@ def y_wing(possibles):
                             if cell not in [(pr, pc), (r1, c1), (r2, c2)]:
                                 if C in possibles[cell[0]][cell[1]]:
                                     possibles[cell[0]][cell[1]].remove(C)
+                                    if len(possibles[cell[0]][cell[1]]) == 1:
+                                        next_rm = next(iter(possibles[cell[0]][cell[1]]))
+                                        rm_from_subgrid(possibles, cell[0], cell[1], next_rm)
+                                        rm_from_rows(possibles, cell[0], cell[1], next_rm)
+                                        rm_from_cols(possibles, cell[0], cell[1], next_rm)
     return possibles
 
 
@@ -255,7 +243,7 @@ def col_elim(possibles, row, exclusions, to_rm):
                 rm_from_cols(possibles, row, col, next_rm)
 
 def row_check(locs):
-    if len(locs) != 2 or len(locs) != 3:
+    if len(locs) != 2 and len(locs) != 3:
         return False
 
     srow, scol = locs[0]
@@ -265,7 +253,7 @@ def row_check(locs):
     return scol
 
 def col_check(locs):
-    if len(locs) != 2 or len(locs) != 3:
+    if len(locs) != 2 and len(locs) != 3:
         return False
 
     srow, scol = locs[0]
@@ -286,143 +274,12 @@ def pointing(possibles):
                 locs = h_map[n]
                 fixed_col = row_check(locs)
                 fixed_row = col_check(locs)
-                if fixed_col != False:
+                if fixed_col is not False:
                     row_elim(possibles, fixed_col, locs, n)
-                if fixed_row != False:
+                if fixed_row is not False:
                     col_elim(possibles, fixed_row, locs, n)
     return possibles
 
-
-
-# y wing
-# def get_row_wing(possibles, wing_col, pivot_col, a_val, c_val):
-#   for r in range(9):
-#       row_wing = possibles[r][pivot_col]
-#       if a_val in row_wing and c_val in row_wing:
-#           # remove c from the intersecting place
-#           possibles[r][wing_col].discard(c_val)
-
-# def get_col_wing(possibles, pivot_set, pivot_row, pivot_col):
-#     for c in range(9):
-#       col_wing = possibles[pivot_row][c]
-#       col_wing = tuple(col_wing)
-#       if len(col_wing) == 2 and col_wing[0] in pivot_set:
-#         a_val = None
-#         for pivot_val in pivot_set:
-#             if pivot_val != col_wing[0]:
-#               a_val = pivot_val
-#         c_val = col_wing[1]
-#         get_row_wing(possibles, c, pivot_col, a_val, c_val)
-#       elif len(col_wing) == 2 and col_wing[1] in pivot_set:
-#         a_val = None
-#         for pivot_val in pivot_set:
-#             if pivot_val != col_wing[1]:
-#               a_val = pivot_val
-#         c_val = col_wing[0]
-#         get_row_wing(possibles, c, pivot_col, a_val, c_val)
-
-# def y_wing(possibles):
-#     for row in range(9):
-#         for col in range(9):
-#             pivot_set = possibles[row][col]
-#             if len(pivot_set) == 2:
-#               get_col_wing(possibles, pivot_set, row, col)
-
-
-# possibles[r][c] is a set of candidates, e.g. {2, 5}
-# def y_wing_search(pr, pc):
-#     # row search
-#     row_viewers = set()
-#     for r in range(9):
-#         if r != pr:
-#             row_viewers.add((r, pc))
-
-#     # column search
-#     col_viewers = set()
-#     for c in range(9):
-#         if c != pc:
-#             row_viewers.add((pr, c))
-
-#     # subgrid search
-#     subgrid_viewers = set()
-#     start_r, start_c = 3 * (pr // 3), 3 * (pc // 3)
-#     for r in range(start_r, start_r + 3):
-#         for c in range(start_c, start_c + 3):
-#             if (r, c) != (pr, pc):
-#                 subgrid_viewers.add((r, c))
-
-#     return row_viewers | col_viewers | subgrid_viewers
-
-# def y_wing(possibles):
-#     # Iterate over every cell as the pivot (2 candidates)
-#     for pr in range(9):
-#         for pc in range(9):
-#             pivot = possibles[pr][pc]
-#             if len(pivot) != 2:
-#                 continue
-
-#             # Find all potential wings that share one candidate with pivot
-#             wings = []
-#             for wr in range(9):
-#                 row_wing = possibles[wr][pc]
-#                 if len(row_wing) == 2 and len(row_wing & pivot) == 1:
-#                     wings.append((wr, pc))
-
-#             for wc in range(9):
-#                 col_wing = possibles[pr][wc]
-#                 if len(col_wing) == 2 and len(col_wing & pivot) == 1:
-#                     wings.append((pr, wc))
-
-#             start_r, start_c = 3 * (pr // 3), 3 * (pc // 3)
-#             print(pivot)
-#             for r in range(start_r, start_r + 3):
-#                 for c in range(start_c, start_c + 3):
-#                     subgrid_wing = possibles[r][c]
-#                     if len(subgrid_wing) == 2 and len(subgrid_wing & pivot) == 1:
-#                         wings.append((r, c))
-
-#             # Try pairing wings to form a Y-Wing
-#             for i in range(len(wings)):
-#                 wing1_row, wing1_col = wings[i]
-#                 wing1 = possibles[wing1_row][wing1_col]
-
-#                 for j in range(i + 1, len(wings)):
-#                     wing2_row, wing2_col = wings[j]
-#                     wing2 = possibles[wing2_row][wing2_col]
-#                     print(wing1)
-#                     print(wing2)
-#                     # Both wings must share exactly one candidate with the pivot, and different ones
-#                     shared = wing1 & wing2
-#                     viewers = y_wing_search(wing1_row, wing1_col) & y_wing_search(wing2_row, wing2_col)
-
-#                     for v_row, v_col in viewers:
-#                         old_len = len(possibles[v_row][v_col])
-#                         possibles[v_row][v_col] = possibles[v_row][v_col] - shared
-#                         print(possibles[v_row][v_col])
-#                         new_len = len(possibles[v_row][v_col])
-#                         if len(possibles[v_row][v_col]) == 1 and old_len != new_len: # might be a problem
-#                             next_rm = next(iter(possibles[v_row][v_col]))
-#                             rm_from_subgrid(possibles, v_row, v_col, next_rm)
-#                             rm_from_rows(possibles, v_row, v_col, next_rm)
-#                             rm_from_cols(possibles, v_row, v_col, next_rm)
-#     return possibles
-
-
-
-
-
-
-# x wing
-
-# def x_wing_helper(possibles, pivot_set, col1, col2):
-#    for row in range(9):
-
-
-# def x_wing(possibles):
-#    for row in range(9):
-#       for i in range(9):
-#          for j in range(i + 1, 9):
-#             if possibles[row][i] == possibles[row][j] and len(possibles[row][i]) == 2:
 
 
 # naked singles
@@ -439,3 +296,87 @@ def pointing(possibles):
 #     return possibles
 
 
+
+
+def x_wing(possibles):
+    size = 9
+    # Row-based X-Wing
+    for candidate in range(1, 10):
+        for r1 in range(size):
+            cols_r1 = [c for c in range(size) if candidate in possibles[r1][c]]
+            if len(cols_r1) != 2:
+                continue
+            for r2 in range(r1 + 1, size):
+                cols_r2 = [c for c in range(size) if candidate in possibles[r2][c]]
+                if len(cols_r2) == 2 and set(cols_r1) == set(cols_r2):
+                    for col in cols_r1:
+                        for r in range(size):
+                            if r != r1 and r != r2 and candidate in possibles[r][col]:
+                                possibles[r][col].remove(candidate)
+                                if len(possibles[r][col]) == 1:
+                                    next_rm = next(iter(possibles[r][col]))
+                                    rm_from_subgrid(possibles, r, col, next_rm)
+                                    rm_from_rows(possibles, r, col, next_rm)
+                                    rm_from_cols(possibles, r, col, next_rm)
+
+    # Column-based X-Wing
+    for candidate in range(1, 10):
+        for c1 in range(size):
+            rows_c1 = [r for r in range(size) if candidate in possibles[r][c1]]
+            if len(rows_c1) != 2:
+                continue
+            for c2 in range(c1 + 1, size):
+                rows_c2 = [r for r in range(size) if candidate in possibles[r][c2]]
+                if len(rows_c2) == 2 and set(rows_c1) == set(rows_c2):
+                    for row in rows_c1:
+                        for c in range(size):
+                            if c != c1 and c != c2 and candidate in possibles[row][c]:
+                                possibles[row][c].remove(candidate)
+                                if len(possibles[row][c]) == 1:
+                                    next_rm = next(iter(possibles[row][c]))
+                                    rm_from_subgrid(possibles, row, c, next_rm)
+                                    rm_from_rows(possibles, row, c, next_rm)
+                                    rm_from_cols(possibles, row, c, next_rm)
+    return possibles
+
+
+def box_line_reduction(board):
+    # Row-wise box-line reduction
+    for row in range(9):
+        for num in range(1, 10):
+            # Find columns in this row where num is a candidate
+            cols_with_num = [col for col in range(9) if num in board[row][col]]
+            if not cols_with_num:
+                continue
+            # Check if all these columns are in the same box
+            box_cols = {col // 3 for col in cols_with_num}
+            if len(box_cols) == 1:
+                box_col_start = box_cols.pop() * 3
+                box_row_start = (row // 3) * 3
+                # Remove num from other rows in the same box columns
+                for r in range(box_row_start, box_row_start + 3):
+                    if r == row:
+                        continue
+                    for c in range(box_col_start, box_col_start + 3):
+                        if num in board[r][c]:
+                            board[r][c].remove(num)
+    # Column-wise box-line reduction
+    for col in range(9):
+        for num in range(1, 10):
+            # Find rows in this column where num is a candidate
+            rows_with_num = [row for row in range(9) if num in board[row][col]]
+            if not rows_with_num:
+                continue
+            # Check if all these rows are in the same box
+            box_rows = {row // 3 for row in rows_with_num}
+            if len(box_rows) == 1:
+                box_row_start = box_rows.pop() * 3
+                box_col_start = (col // 3) * 3
+                # Remove num from other columns in the same box rows
+                for c in range(box_col_start, box_col_start + 3):
+                    if c == col:
+                        continue
+                    for r in range(box_row_start, box_row_start + 3):
+                        if num in board[r][c]:
+                            board[r][c].remove(num)
+    return board
--- test_basic.py
+++ test_basic.py
@@ -2,33 +2,34 @@ import solver
 from tabulate import tabulate
 
 
-puzzle = [
-  [None, 5, None, None, None, None, None, 8, None],
-  [None, None, None, None, 8, 6, None, None, None],
-  [None, None, None, 2, None, 1, None, 7, None],
-  [None, None, 9, None, 2, None, 6, None, 1],
-  [2, 8, None, None, None, None, None, 5, 4],
-  [7, None, 3, None, 6, None, 9, None, None],
-  [None, 9, None, 6, None, 5, None, None, None],
-  [None, None, None, 1, 7, None, None, None, None],
-  [None, 3, None, None, None, None, None, 1, None]
-]
+# puzzle = [
+#   [None, 5, None, None, None, None, None, 8, None],
+#   [None, None, None, None, 8, 6, None, None, None],
+#   [None, None, None, 2, None, 1, None, 7, None],
+#   [None, None, 9, None, 2, None, 6, None, 1],
+#   [2, 8, None, None, None, None, None, 5, 4],
+#   [7, None, 3, None, 6, None, 9, None, None],
+#   [None, 9, None, 6, None, 5, None, None, None],
+#   [None, None, None, 1, 7, None, None, None, None],
+#   [None, 3, None, None, None, None, None, 1, None]
+# ]
 
 def print_possibles(possibles):
   table = [[str(sorted(list(cell))) for cell in row] for row in possibles]
   print(tabulate(table, tablefmt="grid"))
 
 
-possibles = solver.det_possibles(puzzle)
-print_possibles(possibles)
+# possibles = solver.det_possibles(puzzle)
+# print_possibles(possibles)
 
-possibles = solver.hidden_singles(possibles)
-print_possibles(possibles)
+# possibles = solver.hidden_singles(possibles)
+# print_possibles(possibles)
 
-possibles = solver.y_wing(possibles)
-print_possibles(possibles)
+# possibles = solver.y_wing(possibles)
+# print_possibles(possibles)
 
-
+# possibles = solver.hidden_singles(possibles)
+# print_possibles(possibles)
 
 
 # gset = {(8, 8), (7, 7), (6, 7), (7, 6), (6, 8), (6, 6), (8, 7)}
@@ -43,3 +44,30 @@ print_possibles(possibles)
 # print_possibles(possibles)
 # possibles = solver.y_wing(possibles)
 # print_possibles(possibles)
+
+puzzle = [
+  [2, None, None, 3, None, 5, None, None, None],
+  [None, None, 8, 2, 4, None, 3, None, None],
+  [7, None, None, None, None, None, None, None, 2],
+  [5, None, None, 4, None, None, None, 8, None],
+  [None, None, 3, None, 2, None, 9, None, None],
+  [None, 2, None, None, None, 8, None, None, 1],
+  [8, None, None, None, None, None, None, None, 9],
+  [None, None, 7, None, 8, 2, 1, None, None],
+  [None, None, None, 5, None, 1, None, None, 4]
+]
+
+possibles = solver.det_possibles(puzzle)
+print_possibles(possibles)
+possibles = solver.hidden_singles(possibles)
+print_possibles(possibles)
+possibles = solver.col_hidden_singles(possibles)
+print_possibles(possibles)
+possibles = solver.row_hidden_singles(possibles)
+print_possibles(possibles)
+possibles = solver.hidden_singles(possibles)
+print_possibles(possibles)
+possibles = solver.col_hidden_singles(possibles)
+print_possibles(possibles)
+possibles = solver.x_wing(possibles)
+print_possibles(possibles)
